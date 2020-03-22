872140b465c31e1b09529fbbb51e3e84d1a20006
naritotakizawa <toritoritorina@gmail.com>
2020-03-22 09:50:00 +0900

    アプロダ、アプリケーションとモデルの作成

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nuploader1.apps.Nuploader1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('uploader/', include('nuploader1.urls')),
 ]
 
 
