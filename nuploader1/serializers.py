9b9ee02f8b77aed1dc0b51ec4a3abaf626450bb5
naritotakizawa <toritoritorina@gmail.com>
2020-03-26 15:17:48 +0900

    アプロダ、REST APIの実装③

--- nuploader1/serializers.py
+++ nuploader1/serializers.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from .models import Composite
 
@@ -10,12 +12,21 @@ class SimpleCompositeSerializer(serializers.ModelSerializer):
 
 
 class SimpleCompositeRelation(serializers.RelatedField):
+    default_error_messages = {
+        'does_not_exist': _('Invalid pk "{pk_value}" - object does not exist.'),
+        'incorrect_type': _('Incorrect type. Expected pk value, received {data_type}.'),
+    }
 
     def to_representation(self, value):
         return SimpleCompositeSerializer(value).data
 
     def to_internal_value(self, data):
-        return self.get_queryset().get(pk=data)
+        try:
+            return self.get_queryset().get(pk=data)
+        except ObjectDoesNotExist:
+            self.fail('does_not_exist', pk_value=data)
+        except (TypeError, ValueError):
+            self.fail('incorrect_type', data_type=type(data).__name__)
 
     def get_choices(self, cutoff=None):
         queryset = self.get_queryset()
@@ -42,3 +53,33 @@ class CompositeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Composite
         fields = ('pk', 'name', 'is_dir', 'src', 'parent', 'zip_depth', 'composite_set')
+
+    def get_attr(self, name, attrs):
+        value = attrs.get(name)
+        if value is None and self.instance:
+            value = getattr(self.instance, name)
+        return value
+
+    def validate(self, attrs):
+        parent = self.get_attr('parent', attrs)
+        name = self.get_attr('name', attrs)
+        is_dir = self.get_attr('is_dir', attrs)
+        src = self.get_attr('src', attrs)
+
+        if (self.instance and parent) and (parent.pk == self.instance.pk):
+            raise serializers.ValidationError('親ディレクトリが自分です')
+
+        # 同名ファイル・ディレクトリがないかチェック
+        same_names = Composite.objects.filter(parent=parent, name=name)
+        if self.instance:  # 更新の場合は、自分が同名ファイルとして出てくるので、それは除く
+            same_names = same_names.exclude(pk=self.instance.pk)
+        if same_names.exists():
+            raise serializers.ValidationError('同じ名前のファイル・ディレクトリが既に存在します')
+
+        if not is_dir and not src:  # 基本的にはこない
+            raise serializers.ValidationError('ファイルの時は、ファイルを添付してください')
+
+        if is_dir and src:  # 基本的にはこない
+            raise serializers.ValidationError('ディレクトリの時は、ファイルを添付しないでください')
+
+        return attrs
--- nuploader1/views.py
+++ nuploader1/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 from .models import Composite
 from .serializers import CompositeSerializer
 
@@ -6,3 +7,15 @@ from .serializers import CompositeSerializer
 class CompositeViewSet(viewsets.ModelViewSet):
     queryset = Composite.objects.all()
     serializer_class = CompositeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        queryset = Composite.objects.filter(parent__isnull=True)
+        queryset = self.filter_queryset(queryset)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
