0350ff845ee54faaece4904d91e9a1995f9d3500
naritotakizawa <toritoritorina@gmail.com>
2020-03-26 15:19:30 +0900

    アプロダ、REST APIの実装③

--- nuploader1/views.py
+++ nuploader1/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import permissions, viewsets
 from rest_framework.response import Response
 from .models import Composite
 from .serializers import CompositeSerializer
