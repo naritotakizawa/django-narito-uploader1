1b7190692e44e9e897c82b1ef4846db2a212e9e3
naritotakizawa <toritoritorina@gmail.com>
2020-03-31 13:19:28 +0900

    インデント等の修正

--- nuploader1/frontend/src/main.js
+++ nuploader1/frontend/src/main.js
@@ -3,22 +3,22 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
-    const getCookie = name => {
-        if (document.cookie && document.cookie !== '') {
-            for (const cookie of document.cookie.split(';')) {
-                const [key, value] = cookie.trim().split('=');
-                if (key === name) {
-                    return decodeURIComponent(value);
-                }
+const getCookie = name => {
+    if (document.cookie && document.cookie !== '') {
+        for (const cookie of document.cookie.split(';')) {
+            const [key, value] = cookie.trim().split('=')
+            if (key === name) {
+                return decodeURIComponent(value)
             }
         }
-    };
+    }
+}
 
 Vue.config.productionTip = false
 
 Vue.prototype.$http = (url, opts) => fetch(url, opts)
 Vue.prototype.$endpoint = process.env.NODE_ENV === 'production' ?  '/uploader/api/composites/'  : 'http://127.0.0.1:8000/uploader/api/composites/'
-Vue.prototype.$csrfToken = getCookie('csrftoken');
+Vue.prototype.$csrfToken = getCookie('csrftoken')
 
 new Vue({
   router,
