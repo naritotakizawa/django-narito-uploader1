b00be60cecc6f4ca51400fdb991d9036a688d064
naritotakizawa <toritoritorina@gmail.com>
2020-04-03 05:16:15 +0900

    アプロダ、データの作成、更新、削除機能①

---
+++ nuploader1/frontend/src/components/CompositeForm.vue
@@ -0,0 +1,122 @@
+<template>
+    <form @submit.prevent="remove" class="composite-form" v-if="selected.type==='delete'">
+        <p>{{ selected.data.name }}の削除</p>
+        <button type="submit">削除</button>
+        <button @click="close" class="button-link">閉じる</button>
+    </form>
+    <form @submit.prevent="add" class="composite-form" v-else-if="selected.type==='new'">
+        <p>新規作成</p>
+        <input type="text" v-model="form.name" placeholder="名前">
+        <div v-if="selected.data.is_dir">
+            <input type="number" v-model="form.zip_depth" placeholder="ZIP階層">
+        </div>
+        <div v-else>
+            <input type="file" ref="upfile">
+        </div>
+        <button type="submit">送信</button>
+        <button @click="close" class="button-link">閉じる</button>
+    </form>
+    <form @submit.prevent="update" class="composite-form" v-else-if="selected.type==='update'">
+        <p>{{ selected.data.name }}の更新</p>
+        <input type="text" v-model="form.name" placeholder="名前">
+        <input type="text" v-model="form.parent" placeholder="親ディレクトリID">
+        <div v-if="selected.data.is_dir">
+            <input type="number" v-model="form.zip_depth" placeholder="ZIP階層">
+        </div>
+        <div v-else>
+            <input type="file" ref="upfile">
+        </div>
+        <button type="submit">更新</button>
+        <button @click="close" class="button-link">閉じる</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'composite-form',
+        props: {
+            selected: {type: Object},
+        },
+        data() {
+            return {
+                form: {
+                    name: '',
+                    zip_depth: 0,
+                    parent: '',
+                }
+            }
+        },
+        mounted() {
+            if (this.selected.type === 'update') {
+                this.form.name = this.selected.data.name
+                this.form.zip_depth = this.selected.data.zip_depth
+                this.form.parent = this.selected.data.parent ? this.selected.data.parent : ''
+            }
+        },
+        methods: {
+            add() {
+                this.$emit('done')
+            },
+            update() {
+                this.$emit('done')
+            },
+            remove() {
+                this.$emit('done')
+            },
+            close() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    .composite-form {
+        width: 300px;
+    }
+
+    .composite-form > * {
+        margin-bottom: 1em;
+    }
+
+    .composite-form > p {
+        font-size: 24px;
+    }
+
+    input[type=text], input[type=email], input[type=number],
+    select, textarea {
+        width: 100%;
+        padding: 10px 10px;
+        box-sizing: border-box;
+        border-radius: 4px;
+        border: solid 1px #ccc;
+    }
+
+    button, a.button {
+        width: 100%;
+        font-size: 14px;
+        -webkit-appearance: none;
+        padding: 10px 16px;
+        border-radius: 4px;
+        background-color: #0366d6;
+        color: #fff;
+        border: solid 1px #0366d6;
+        vertical-align: bottom;
+        box-sizing: border-box;
+        display: inline-block;
+        text-decoration: none;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    button:hover, a.button:hover {
+        opacity: 0.5;
+    }
+
+    .button-link {
+        border: none;
+        background: none;
+        color: #000;
+    }
+</style>
--- nuploader1/frontend/src/components/CompositeList.vue
+++ nuploader1/frontend/src/components/CompositeList.vue
@@ -1,38 +1,59 @@
 <template>
     <section id="composites">
 
-        <!-- 親ディレクトリの表示 -->
-        <div v-if="current.parent" class="parent composite-wrapper" :key="current.parent.pk">
-            <p class="composite" @click="moveBefore">{{ current.parent.name }}</p>
-        </div>
-        <div v-else-if="current.pk" class="parent composite-wrapper" :key="-1">
-            <p class="composite" @click="moveBefore">home</p>
+        <div id="list">
+            <!-- 親ディレクトリの表示 -->
+            <div v-if="current.parent" class="parent composite-wrapper" :key="current.parent.pk">
+                <p class="composite" @click="moveBefore">{{ current.parent.name }}</p>
+            </div>
+            <div v-else-if="current.pk" class="parent composite-wrapper" :key="-1">
+                <p class="composite" @click="moveBefore">home</p>
+            </div>
+
+            <!-- カレントディレクトリの表示 -->
+            <button type="button" @click="createFile">ファイル作成</button>
+            <button type="button" @click="createDir">ディレクトリ作成</button>
+            <div v-if="current.pk" class="current composite-wrapper" :key="current.pk">
+                <p class="composite">{{ current.name }}</p>
+            </div>
+            <div v-else class="current composite-wrapper" :key="-1">
+                <p class="composite">home</p>
+            </div>
+
+            <!-- 子ファイル・ディレクトリの表示 -->
+            <div class="child composite-wrapper" v-for="composite of current.composite_set" :key="composite.pk">
+                <p class="composite" @click="move(composite)">{{ composite.name }}</p>
+                <button type="button" @click="update(composite)">更新</button>
+                <button type="button" @click="remove(composite)">削除</button>
+            </div>
         </div>
 
-        <!-- カレントディレクトリの表示 -->
-        <div v-if="current.pk" class="current composite-wrapper" :key="current.pk">
-            <p class="composite">{{ current.name }}</p>
-        </div>
-        <div v-else class="current composite-wrapper" :key="-1">
-            <p class="composite">home</p>
+        <div id="form">
+            <composite-form :selected="selected" @done="reload" @close="close"
+                            :key="selected.type + '-' + selected.data.pk"></composite-form>
         </div>
 
-        <!-- 子ファイル・ディレクトリの表示 -->
-        <div class="child composite-wrapper" v-for="composite of current.composite_set" :key="composite.pk">
-            <p class="composite" @click="move(composite)">{{ composite.name }}</p>
-        </div>
     </section>
 </template>
 
 <script>
+    import CompositeForm from "./CompositeForm"
+
     export default {
         name: 'composite-list',
+        components: {
+            CompositeForm
+        },
         props: {
             path: {type: String},
         },
         data() {
             return {
                 current: {},
+                selected: {
+                    type: null,
+                    data: {}
+                },
             }
         },
         created() {
@@ -112,30 +133,102 @@
                     this.getCompositeListFromPk(composite.pk)
                 }
             },
-
+            update(composite) {
+                this.selected.data = composite
+                this.selected.type = 'update'
+            },
+            remove(composite) {
+                this.selected.data = composite
+                this.selected.type = 'delete'
+            },
+            createFile(composite) {
+                this.selected.data = {
+                    is_dir: false,
+                    parent: composite.pk ? composite.pk : '',
+                }
+                this.selected.type = 'new'
+            },
+            createDir(composite) {
+                this.selected.data = {
+                    is_dir: true,
+                    parent: composite.pk ? composite.pk : '',
+                }
+                this.selected.type = 'new'
+            },
+            reload() {
+                this.selected.type = null
+                this.selected.data = {}
+                if (this.current.pk) {
+                    this.getCompositeListFromPk(this.current.pk)
+                } else {
+                    this.getCompositeListTop()
+                }
+            },
+            close() {
+                this.selected.data = {}
+                this.selected.type = null
+            }
         },
 
     }
 </script>
 
+
 <style scoped>
     .composite-wrapper {
         margin-bottom: 50px;
     }
+
     .current {
         background-color: #eee;
         padding: 6px;
+    }
+
+    #form {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: rgba(255, 255, 255, 0.9);
+        padding: 10px;
     }
 
     @media (min-width: 1024px) {
         #composites {
             padding-top: 120px;
         }
+
         .current > .composite {
             margin-left: 50px;
         }
+
         .child {
             margin-left: 100px;
+        }
+    }
+
+    @media (min-width: 1366px) {
+        #composites {
+            padding-top: 120px;
+            display: grid;
+            grid-template-columns: 700px 1fr;
+        }
+
+        #list {
+            grid-column: 1;
+        }
+
+        #form {
+            grid-column: 2;
+            justify-self: center;
+            margin-top: 100px;
+            position: static;
+            top: 0;
+            left: 0;
+            transform: none;
+            background-color: transparent;
+            padding: 0;
+            box-shadow: none;
         }
     }
 </style>
